bfde101bded668907b941dca3a01b411afd44699
Siyue Chen <chensiyue98@hotmail.com>
2023-05-02 15:21:26 +0200

    rm fasttext

--- pages/api/class.py
+++ pages/api/class.py
@@ -1,11 +1,11 @@
 from http.server import BaseHTTPRequestHandler
-from fasttext import load_model
+# from fasttext import load_model
 
  
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
-        classifier = load_model("model/model_commits_v2_quant.bin")
+        # classifier = load_model("model/model_commits_v2_quant.bin")
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
